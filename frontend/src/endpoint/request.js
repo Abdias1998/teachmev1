f0c8b85b57b91677f0b5726a4d15888b4306ea34
Abdias Dev <128239881+Abdias1998@users.noreply.github.com>
2025-09-30 12:33:02 +0100

    Update request.js

--- frontend/src/endpoint/request.js
+++ frontend/src/endpoint/request.js
@@ -2,7 +2,7 @@ import React from "react";
 import axios from "axios";
 
 // const api = "https://teachme-1xw7.onrender.com/v1";
-const api = "http://localhost:7200/v1";
+const api = "https://teachme-1xw7.onrender.com/v1";
 
 const request = {
   register: `${api}/auth/register`,
